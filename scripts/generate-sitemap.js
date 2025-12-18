f9df3296970279a1038e5fd392470519738fdc1b
Antonio Macaluso <jockbaia@gmail.com>
2025-12-19 00:57:17 +0100

    feat: sitemap generator

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,39 @@
+// scripts/generate-sitemap.js
+const fs = require('fs');
+const path = require('path');
+
+const DATA_DIRECTORY = path.join(process.cwd(), 'data', 'posts');
+const SITE_URL = 'https://jockbaia.com';
+
+function getMarkdownFileNames() {
+  if (!fs.existsSync(DATA_DIRECTORY)) return [];
+  return fs.readdirSync(DATA_DIRECTORY).filter((file) => file.endsWith('.md'));
+}
+
+function getAllUrls() {
+  const fileNames = getMarkdownFileNames();
+  const postUrls = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, '');
+    return `${SITE_URL}/${id}`;
+  });
+  const staticUrls = [SITE_URL, `${SITE_URL}/blog`, `${SITE_URL}/scuderia`];
+  return [...staticUrls, ...postUrls];
+}
+
+function generateSitemap(urls) {
+  return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls
+    .map((url) => `  <url>\n    <loc>${url}</loc>\n  </url>`)
+    .join('\n')}\n</urlset>`;
+}
+
+function writeSitemap() {
+  const urls = getAllUrls();
+  const sitemap = generateSitemap(urls);
+  const publicDir = path.join(process.cwd(), 'public');
+  if (!fs.existsSync(publicDir)) {
+    fs.mkdirSync(publicDir);
+  }
+  fs.writeFileSync(path.join(publicDir, 'sitemap.xml'), sitemap);
+}
+
+writeSitemap();
